22e65335cae6af213057f5ed0a41663fa8b345d9
Tinh Tran <tttinh@sk-global.biz>
2023-06-16 10:51:38 +0700

    repo: :rocket: introduce new app using svelte

---
+++ src/lib/components/LogoutButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import { Button } from '$components';
+
+	const handleLogout = async () => {
+		const response = await fetch('/api/auth/logout', {
+			method: 'POST',
+			headers: {
+				accept: 'application/json'
+			}
+		});
+		if (response.ok) {
+			invalidateAll();
+		}
+	}
+</script>
+
+<form
+	method="POST"
+	action="/api/auth/logout"
+	on:submit|preventDefault={handleLogout}
+>
+	<Button element="button" type="submit">Logout</Button>
+</form>
---
+++ svelte.config.js
@@ -0,0 +1,28 @@
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			prependData: '@use "src/styles/functions";@use "@unsass/breakpoint";'
+		}
+	}),
+
+	kit: {
+		adapter: adapter({
+      edge: true
+    }),
+		alias: {
+			$components: 'src/lib/components',
+			$assets: 'src/assets',
+			$actions: 'src/lib/actions',
+			$helpers: 'src/lib/helpers',
+			$stores: 'src/lib/stores'
+		}
+	}
+};
+
+export default config;
